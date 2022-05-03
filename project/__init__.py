ed734c020179490a0a35f2a131eeadb51b4a7178
RedGradient <example@mail.com>
2022-05-04 09:45:40 +1100

    added search to admin

--- project/__init__.py
+++ project/__init__.py
@@ -5,7 +5,7 @@ import os
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_perm import Perm
-from flask_migrate import Migrate
+# from flask_migrate import Migrate
 from flask_admin.menu import MenuLink
 
 
@@ -34,7 +34,7 @@ db = SQLAlchemy(app)  # объект базы данных
     python3 -m flask db migrate -m "Initial migration."
     python3 -m flask db upgrade
 """
-migrate = Migrate(app, db)
+# migrate = Migrate(app, db)
 
 login_manager = LoginManager(app)
 
@@ -58,17 +58,52 @@ def load_current_user():
     return current_user
 
 
-class MyModelView(ModelView):
+class UserView(ModelView):
     # поле time_created не будет показываться в админ-панели
     form_excluded_columns = ('time_created',)
+    column_searchable_list = ['id', 'username', 'time_created', 'is_staff']
+    column_filters = ['id', 'username', 'time_created', 'is_staff']
+    page_size = 50
+
+
+class ReaderView(ModelView):
+    # поле time_created не будет показываться в админ-панели
+    form_excluded_columns = ('time_created', )
+    column_searchable_list = ['id', 'user_id', 'schoolyear', 'firstname', 'lastname', 'year_of_birth', 'grade', 'time_created']
+    column_filters = ['id', 'user_id', 'schoolyear', 'firstname', 'lastname', 'year_of_birth', 'grade', 'time_created']
+    page_size = 50
+
+
+class BookCardView(ModelView):
+    # поле time_created не будет показываться в админ-панели
+    form_excluded_columns = ('time_created',)
+    column_searchable_list = ['id', 'author', 'title', 'publisher', 'publishing_year', 'description']
+    column_filters = ['id', 'author', 'title', 'publisher', 'publishing_year', 'description']
+    page_size = 50
+
+
+class ReadersBookCardsView(ModelView):
+    # поле time_created не будет показываться в админ-панели
+    form_excluded_columns = ('time_created',)
+    column_searchable_list = ['id', 'reader_id', 'bookcard_id', 'deadline', 'deadline']
+    column_filters = ['id', 'reader_id', 'bookcard_id', 'deadline', 'deadline']
+    page_size = 50
+
+
+class PostView(ModelView):
+    # поле time_created не будет показываться в админ-панели
+    form_excluded_columns = ('time_created',)
+    column_searchable_list = ['id', 'author_id', 'title', 'body', 'time_created']
+    column_filters = ['id', 'author_id', 'title', 'body', 'time_created']
+    page_size = 50
 
 
 # добавляем наши модели в админ-панель
-admin.add_view(MyModelView(User, db.session))
-admin.add_view(MyModelView(Reader, db.session))
-admin.add_view(MyModelView(BookCard, db.session))
-admin.add_view(MyModelView(ReadersBookCards, db.session))
-admin.add_view(MyModelView(Post, db.session))
+admin.add_view(UserView(User, db.session))
+admin.add_view(ReaderView(Reader, db.session))
+admin.add_view(BookCardView(BookCard, db.session))
+admin.add_view(ReadersBookCardsView(ReadersBookCards, db.session))
+admin.add_view(PostView(Post, db.session))
 
 # создаем базу данных, если ее нет
 db.create_all()
